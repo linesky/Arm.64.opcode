bfa586ceb41d0c3fc30130522c13b9bbcabe36e8
sky line <32512284+linesky@users.noreply.github.com>
2024-10-22 13:37:58 +0100

    Add files via upload

--- list.sh
+++ list.sh
@@ -1,1 +1,1 @@
-cat $1 | grep -v "rror" | grep -v "messages"| grep -v "No such file" | grep -v -e '^$'
+cat $1 | grep -v "rror" | grep -v "message"| grep -v "nfo"| grep -v "not found" |grep -v "No such file" | grep -v -e '^$'
