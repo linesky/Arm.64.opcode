6126cbd6299e11b59eed5305fdad2b634e00b561
sky line <32512284+linesky@users.noreply.github.com>
2024-11-11 08:32:56 +0000

    Add files via upload

---
+++ debug.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class MainApp
+{
+    static void ddebugs(int a){
+        string s="";
+        Console.WriteLine(a);
+        s=Console.ReadLine("\nwait a key enter.");
+        s=s+"";
+     }
+
+    public static void Main()
+    {
+        int i=0;
+        for(i=0;i<10;i++)ddebugs(i);
+
+    }
+}
+
+
+
---
+++ mods.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class MainApp
+{
+    static int mods(int a,int b){
+        return a%b;
+    }
+
+
+    public static void Main()
+    {
+        Console.WriteLine(mods(17,8));
+    }
+}
+
